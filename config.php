cde65047b237c16acfb122553320a9ec01d807ef
RAMKUSH MAURYA <ramkushmauryait@gmail.com>
2022-01-06 08:27:44 +0530

    8th commit

--- config.php
+++ config.php
@@ -29,7 +29,7 @@ class Config {
     // ------------------------------------------------------------------------
     // GENERAL SETTINGS
     // ------------------------------------------------------------------------
-    const BASE_URL      = 'https://fintech-appointments.herokuapp.com';
+    const BASE_URL      = 'http://fintech-appointments.herokuapp.com';
     const LANGUAGE      = 'english';
     const DEBUG_MODE    = FALSE;
 
