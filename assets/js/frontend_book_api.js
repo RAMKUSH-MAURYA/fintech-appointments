7a21785c1438e1dc2ba099175081d4df3c31c75c
RAMKUSH MAURYA <ramkushmauryait@gmail.com>
2022-01-06 23:45:59 +0530

    commented code to fix issue

--- assets/js/frontend_book_api.js
+++ assets/js/frontend_book_api.js
@@ -262,9 +262,11 @@ window.FrontendBookApi = window.FrontendBookApi || {};
             dataType: 'json'
         })
             .done(function (response) {
-                unavailableDatesBackup = response;
-                selectedDateStringBackup = selectedDateString;
-                applyUnavailableDates(response, selectedDateString, true);
+                // unavailableDatesBackup = response;
+                // selectedDateStringBackup = selectedDateString;
+                // applyUnavailableDates(response, selectedDateString, true);
+            }).catch(function(){
+                
             });
     };
 
